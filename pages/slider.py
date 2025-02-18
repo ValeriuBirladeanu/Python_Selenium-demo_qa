63728a2efaf0629025a8994cb4318ad198e6ef1f
Valeriu Birlaeanu <valeriu.birladeanu@gmail.com>
2025-02-18 17:07:01 +0200

    Adding page content slider.py

---
+++ pages/slider.py
@@ -0,0 +1,25 @@
+import random
+
+import allure
+from base.base_page import BasePage
+from config.urls import Urls
+
+
+class Slider(BasePage):
+    PAGE_URL = Urls.SLIDER
+
+    INPUT_SLIDER = ("xpath", '//input[@class="range-slider range-slider--primary"]')
+    VALUE_SLIDER = ("xpath", '//input[@id="sliderValue"]')
+
+    @allure.step("Change slider value")
+    def change_slider_value(self):
+        self.value_before = self.element_is_visible(self.VALUE_SLIDER).get_attribute('value')
+        print(self.value_before)
+        input_slider = self.element_is_visible(self.INPUT_SLIDER)
+        self.action_drag_and_drop_by_offset(input_slider, random.randint(1,100), 0)
+        self.value_after = self.element_is_presence(self.VALUE_SLIDER).get_attribute('value')
+        print(self.value_after)
+
+    @allure.step("Check opened new accordian(")
+    def check_slider_value_change(self):
+        self.verify_value_change(self.value_before, self.value_after)
